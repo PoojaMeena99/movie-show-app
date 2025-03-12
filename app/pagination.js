d3d70f5147ec2bf6f3ff9775e542aa23b9ef876b
Pooja Meena <poojameena21@navgurukul.org>
2025-03-13 02:51:02 +0530

    Pagination CSS Fix

--- app/pagination.js
+++ app/pagination.js
@@ -1,12 +1,12 @@
 function Pagination({ handlePrevious, handleNext, currentPage, totalPages }) {
     return (
         <div className="pagination">
-            <div className="pagination">
+            <div className="pagination_div">
                 <button className="prev" onClick={handlePrevious} disabled={currentPage === 1}>
                     <span>&lt;</span> Previous
                 </button>
                 <span className="current-page">{currentPage}</span>
-                <span className="total-pages">of {totalPages}</span>
+                <span className="total-pages"> / {totalPages}</span>
                 <button className="next" onClick={handleNext} disabled={currentPage === totalPages}>
                     Next <span>&gt;</span>
                 </button>
