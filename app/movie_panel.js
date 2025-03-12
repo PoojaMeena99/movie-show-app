db7fafe350c47812d7fa0a5fc3b1e5f492e9af5c
Pooja Meena <poojameena21@navgurukul.org>
2025-03-13 01:56:00 +0530

    Add all functionality: rating, details, search, and pagination (All are Working)

--- app/movie_panel.js
+++ app/movie_panel.js
@@ -36,46 +36,6 @@ export default Movie_panel;
 
 
 
-// import Link from 'next/link';
-// import MovieItem from "./movie_item";
-
-// function MoviePanel({ data }) {
-//   if (data.length === 1 && data[0].message) {
-//     return (
-//       <div className="no-results">
-//         <p>{data[0].message}</p>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="row movie-penal-row">
-//       {data.map((dataItem) => (
-//         <div className="col-4 movie-item-div" key={dataItem.imdbID}>
-//           <Link href={`/?imdbID=${dataItem.imdbID}`}>
-//             <MovieItem singleData={dataItem} />
-//           </Link>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default MoviePanel;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ function Page() {
   const [currentPage, setCurrentPage] = useState(1);
   const [checkMovieSerialId, setCheckMovieSerialId] = useState(null);
 
-  
+
   function createCard(searchedFetchedData, inputSearch) {
     if (inputSearch.trim().length >= 2) {
       if (searchedFetchedData.length === 0) {
@@ -70,111 +70,6 @@ function Page() {
 }
 
 export default Page;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use client";
-// import { React, useState } from "react";
-// import HardCodeData from "./movies_data";
-// import Header from "./header.js";
-// import Movie_panel from "./movie_panel";
-// import Pagination from './pagination';
-// import DetailPage from './details_page';
-// import { useSearchParams } from 'next/navigation';
-
-// function Page() {
-//   const [data, setData] = useState(HardCodeData);
-//   const [currentPage, setCurrentPage] = useState(1);
-  
-//   const searchParams = useSearchParams();
-//   const imdbID = searchParams.get("imdbID");
-
-//   const create_cards = (searchedFetchedData, inputSearch) => {
-
-//     if (inputSearch.trim().length >= 2) {
-//       if (searchedFetchedData.length === 0) {
-//         setData([{ message: "Movie is not found" }]);
-//       } else {
-//         setData(searchedFetchedData);
-//       }
-//     } else {
-//       setData(HardCodeData);
-//     }
-//   };
-
-//   let perPageData = 9
-
-//   const current_page_movies = data.slice((currentPage - 1) * perPageData, currentPage * perPageData);
-//   const totalPages = Math.ceil(data.length / perPageData);
-
-//   const handlePrevious = function () {
-//     if (currentPage > 1) {
-//       setCurrentPage(currentPage - 1);
-//     }
-//   };
-
-//   const handleNext = function () {
-//     if (currentPage < totalPages) {
-//       setCurrentPage(currentPage + 1);
-//     }
-//   };
-//   let componentRender;
-//   if (imdbID) {
-//     componentRender = <DetailPage />;
-//   } else {
-//     componentRender = (
-//       <>
-//         <Movie_panel data={current_page_movies} />
-//         <Pagination
-//           handlePrevious={handlePrevious}
-//           handleNext={handleNext}
-//           currentPage={currentPage}
-//           totalPages={totalPages}
-//         />
-//       </>
-//     );
-//   }
-
-//   return (
-//     <div className="container">
-//       <Header create_cards={create_cards} />
-//       {componentRender}
-//     </div>
-//   );
-// }
-
-// export default Page;
-
-
-
-
-
-
-
-
-
 
 
 
